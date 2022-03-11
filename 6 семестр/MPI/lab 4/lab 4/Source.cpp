7a85396588c3e29c4eef408df2b8d99d2042077d
Maitrog <35650757+Maitrog@users.noreply.github.com>
2022-03-11 22:11:12 +0300

    Add mpi lab 4

--- 6 семестр/MPI/lab 4/lab 4/Source.cpp
+++ 6 семестр/MPI/lab 4/lab 4/Source.cpp
@@ -4,12 +4,19 @@
 #include <mpi.h>
 #include <cstddef>
 
+
+constexpr int MAX_SIZE = 1000;
 constexpr int SIZE = 10;
+
 struct LONG_INT {
 	int size;
 	int base;
-	int number[SIZE];
+	int number[MAX_SIZE];
 };
+
+LONG_INT multiply(LONG_INT left, LONG_INT right);
+void printLongInt(LONG_INT x);
+int getNewSize(LONG_INT x);
 
 int main(int argc, char** argv) {
 
@@ -20,60 +27,95 @@ int main(int argc, char** argv) {
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	constexpr int n_structure_per_process = 4; // M = 5
-
-	// Random generator init
-	srand(rank * 10);
-
-	// Creating the dataset
-	LONG_INT data[n_structure_per_process];
-
-	// Generating the data
-	for (int i = 0; i < n_structure_per_process; ++i) {
-		// Terrible way of generating random numbers, don't reproduce this at home
-		data[i].size = SIZE;
-		data[i].base = 1;
-		for (int j = 0; j < SIZE; ++j)
-			data[i].number[j] = (j < data[i].size ? rand() % 10 : 0.0);
-	}
-
-	// 1- Here, create all the properties to call MPI_Type_create_struct
-	MPI_Aint displacements[3] = {offsetof(LONG_INT, size), offsetof(LONG_INT, base), offsetof(LONG_INT, number)};
-	int block_lengths[3] = {1, 1, SIZE};
-	MPI_Datatype types[3] = {MPI_INT, MPI_INT, MPI_INT};
-	MPI_Datatype custom_dt;
-	
-	// 2- Create the type, and commit it
-	MPI_Type_create_struct(3, block_lengths, displacements, types, &custom_dt);
-	MPI_Type_commit(&custom_dt);
-	// Gathering the data
-	LONG_INT* gathered_data = nullptr;
+	int n_structure_per_process = 4;
+	int n_structure = n_structure_per_process * size;
+	LONG_INT* data = nullptr;
 
 	if (rank == 0)
-		gathered_data = new LONG_INT[n_structure_per_process * size];
-
-	MPI_Gather(data, n_structure_per_process, custom_dt,
-		gathered_data, n_structure_per_process, custom_dt, 0, MPI_COMM_WORLD);
-
-	// And printing it
-	if (rank == 0) {
-		for (int i = 0; i < size; ++i) {
-			for (int j = 0; j < n_structure_per_process; ++j) {
-				int data_id = i * n_structure_per_process + j; // Linear index
-
-				std::cout << "Data structure " << data_id << " : [";
-				int N = gathered_data[data_id].size;
-
-				for (int k = 0; k < N; ++k) {
-					std::cout << gathered_data[data_id].number[k] << (k == N - 1 ? "]" : "; ");
-				}
-				std::cout << std::endl;
+	{
+		srand(rank * 10);
+		data = new LONG_INT[n_structure];
+		for (int i = 0; i < n_structure; ++i) {
+			data[i].size = SIZE;
+			data[i].base = 10;
+			for (int j = 0; j < MAX_SIZE; ++j) {
+				data[i].number[j] = (j < data[i].size ? rand() % 10 : 0);
 			}
+			std::cout << i << ": ";
+			printLongInt(data[i]);
+			std::cout << std::endl;
 		}
 	}
 
+	MPI_Aint displacements[3] = { offsetof(LONG_INT, size), offsetof(LONG_INT, base), offsetof(LONG_INT, number) };
+	int block_lengths[3] = { 1, 1, MAX_SIZE };
+	MPI_Datatype types[3] = { MPI_INT, MPI_INT, MPI_INT };
+	MPI_Datatype custom_dt;
+
+	MPI_Type_create_struct(3, block_lengths, displacements, types, &custom_dt);
+	MPI_Type_commit(&custom_dt);
+
+	LONG_INT* gathered_data = nullptr;
+	LONG_INT* scatter_data = new LONG_INT[n_structure_per_process];
+	LONG_INT result;
+	result.size = 1;
+	result.base = 10;
+	for (int j = 0; j < MAX_SIZE; ++j)
+		result.number[j] = (j == 0 ? 1 : 0);
+
+	if (rank == 0)
+		gathered_data = new LONG_INT[size];
+
+	MPI_Scatter(data, n_structure_per_process, custom_dt, scatter_data, n_structure_per_process, custom_dt, 0, MPI_COMM_WORLD);
+	for (int i = 0; i < n_structure_per_process; i++) {
+		result = multiply(result, scatter_data[i]);
+	}
+
+	MPI_Gather(&result, 1, custom_dt, gathered_data, 1, custom_dt, 0, MPI_COMM_WORLD);
+
+	if (rank == 0) {
+		result = gathered_data[0];
+		for (int i = 1; i < size; ++i) {
+			result = multiply(result, gathered_data[i]);
+		}
+		printLongInt(result);
+		std::cout << std::endl;
+	}
 
 	MPI_Finalize();
 
 	return 0;
+}
+
+LONG_INT multiply(LONG_INT left, LONG_INT right) {
+	LONG_INT result;
+	result.size = left.size + right.size;
+	result.base = 10;
+	for (int j = 0; j < MAX_SIZE; ++j)
+		result.number[j] = 0;
+
+	for (int i = 0; i < left.size; i++) {
+		int carry = 0;
+		for (int j = 0; j < right.size || carry != 0; j++) {
+			int cur = result.number[i + j] + left.number[i] * (j < right.size ? right.number[j] : 0) + carry;
+			result.number[i + j] = cur % base;
+			carry = cur / result.base;
+		}
+	}
+	result.size = getNewSize(result);
+	return result;
+}
+
+int getNewSize(LONG_INT x) {
+	int newSize = x.size;
+	while (x.number[newSize - 1] == 0) {
+		newSize--;
+	}
+	return newSize;
+}
+
+void printLongInt(LONG_INT x) {
+	for (int i = x.size - 1; i >= 0; --i) {
+		std::cout << x.number[i];
+	}
 }
