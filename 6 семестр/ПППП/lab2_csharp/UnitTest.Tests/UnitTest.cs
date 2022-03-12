10020cf61e829ea805f69def650ba1b05fb551f8
Maitrog <35650757+Maitrog@users.noreply.github.com>
2022-03-12 21:33:56 +0300

    Update PPPP lab 2

--- 6 семестр/ПППП/lab2_csharp/UnitTest.Tests/UnitTest.cs
+++ 6 семестр/ПППП/lab2_csharp/UnitTest.Tests/UnitTest.cs
@@ -8,293 +8,266 @@ namespace UnitTest.Tests
 {
     public class UnitTest
     {
+        private readonly StringWriter _stringWriter;
+        public UnitTest()
+        {
+            _stringWriter = new StringWriter();
+            Console.SetOut(_stringWriter);
+        }
 
         [Fact]
         public void IfAllInputArgumentsZero_ThenWriteFive()
         {
             // Arrange
-            var stringWriter = new StringWriter();
-            Console.SetOut(stringWriter);
-
-            string result = "0\n0\n0\n0\n0\n0";
-            var stringReader = new StringReader(result);
+            string input = "0\n0\n0\n0\n0\n0";
+            var stringReader = new StringReader(input);
             Console.SetIn(stringReader);
 
             // Act
             Program.Main(Array.Empty<string>());
 
             // Assert
-            Assert.Equal("5", stringWriter.ToString());
+            Assert.Equal("5", _stringWriter.ToString());
         }
 
         [Fact]
-        public void Test2()
+        public void IfSystemHasSingleSolution_ThenWriteTwoAndResult()
         {
             // Arrange
-            var stringWriter = new StringWriter();
-            Console.SetOut(stringWriter);
-
-            string result = "5\n3\n4\n3\n6\n9";
-            var stringReader = new StringReader(result);
+            string input = "5\n3\n4\n3\n6\n9";
+            var stringReader = new StringReader(input);
             Console.SetIn(stringReader);
 
             // Act
             Program.Main(Array.Empty<string>());
 
             // Assert
-            Assert.Equal("2 -3 7", stringWriter.ToString());
+            Assert.Equal("2 -3 7", _stringWriter.ToString());
         }
 
         [Fact]
         public void IfABCEZeroAndSystemHasSolution_ThenWriteFourAndRezult()
         {
             // Arrange
-            var stringWriter = new StringWriter();
-            Console.SetOut(stringWriter);
-
-            string result = "0\n0\n0\n3\n0\n9";
-            var stringReader = new StringReader(result);
+            string input = "0\n0\n0\n3\n0\n9";
+            var stringReader = new StringReader(input);
             Console.SetIn(stringReader);
 
             // Act
             Program.Main(Array.Empty<string>());
 
             // Assert
-            Assert.Equal("4 3", stringWriter.ToString());
+            Assert.Equal("4 3", _stringWriter.ToString());
         }
 
         [Fact]
         public void IfACDFZeroAndSystemHasSolution_ThenWriteFourAndRezult()
         {
             // Arrange
-            var stringWriter = new StringWriter();
-            Console.SetOut(stringWriter);
-
-            string result = "0\n3\n0\n0\n9\n0";
-            var stringReader = new StringReader(result);
+            string input = "0\n3\n0\n0\n9\n0";
+            var stringReader = new StringReader(input);
             Console.SetIn(stringReader);
 
             // Act
             Program.Main(Array.Empty<string>());
 
             // Assert
-            Assert.Equal("4 3", stringWriter.ToString());
+            Assert.Equal("4 3", _stringWriter.ToString());
         }
 
         [Fact]
         public void IfACZeroAndSystemHasSolution_ThenWriteFourAndRezult()
         {
             // Arrange
-            var stringWriter = new StringWriter();
-            Console.SetOut(stringWriter);
-
-            string result = "0\n3\n0\n3\n9\n9";
-            var stringReader = new StringReader(result);
+            string input = "0\n3\n0\n3\n9\n9";
+            var stringReader = new StringReader(input);
             Console.SetIn(stringReader);
 
             // Act
             Program.Main(Array.Empty<string>());
 
             // Assert
-            Assert.Equal("4 3", stringWriter.ToString());
+            Assert.Equal("4 3", _stringWriter.ToString());
         }
 
         [Fact]
         public void IfABDEZeroAndSystemHasSolution_ThenWriteThreeAndRezult()
         {
             // Arrange
-            var stringWriter = new StringWriter();
-            Console.SetOut(stringWriter);
-
-            string result = "0\n0\n3\n0\n0\n9";
-            var stringReader = new StringReader(result);
+            string input = "0\n0\n3\n0\n0\n9";
+            var stringReader = new StringReader(input);
             Console.SetIn(stringReader);
 
             // Act
             Program.Main(Array.Empty<string>());
 
             // Assert
-            Assert.Equal("3 3", stringWriter.ToString());
+            Assert.Equal("3 3", _stringWriter.ToString());
         }
 
         [Fact]
         public void IfBCDFZeroAndSystemHasSolution_ThenWriteThreeAndRezult()
         {
             // Arrange
-            var stringWriter = new StringWriter();
-            Console.SetOut(stringWriter);
-
-            string result = "3\n0\n0\n0\n9\n0";
-            var stringReader = new StringReader(result);
+            string input = "3\n0\n0\n0\n9\n0";
+            var stringReader = new StringReader(input);
             Console.SetIn(stringReader);
 
             // Act
             Program.Main(Array.Empty<string>());
 
             // Assert
-            Assert.Equal("3 3", stringWriter.ToString());
+            Assert.Equal("3 3", _stringWriter.ToString());
         }
 
         [Fact]
         public void IfBDZeroAndSystemHasSolution_ThenWriteThreeAndRezult()
         {
             // Arrange
-            var stringWriter = new StringWriter();
-            Console.SetOut(stringWriter);
-
-            string result = "3\n0\n3\n0\n9\n9";
-            var stringReader = new StringReader(result);
+            string input = "3\n0\n3\n0\n9\n9";
+            var stringReader = new StringReader(input);
             Console.SetIn(stringReader);
 
             // Act
             Program.Main(Array.Empty<string>());
 
             // Assert
-            Assert.Equal("3 3", stringWriter.ToString());
+            Assert.Equal("3 3", _stringWriter.ToString());
         }
 
         [Fact]
         public void IfABEZeroAndSystemHasSolution_ThenWriteOneAndRezult()
         {
             // Arrange
-            var stringWriter = new StringWriter();
-            Console.SetOut(stringWriter);
-
-            string result = "0\n0\n3\n3\n0\n9";
-            var stringReader = new StringReader(result);
+            string input = "0\n0\n3\n3\n0\n9";
+            var stringReader = new StringReader(input);
             Console.SetIn(stringReader);
 
             // Act
             Program.Main(Array.Empty<string>());
 
             // Assert
-            Assert.Equal("1 -1 3", stringWriter.ToString());
+            Assert.Equal("1 -1 3", _stringWriter.ToString());
         }
 
         [Fact]
         public void IfCDFZeroAndSystemHasSolution_ThenWriteOneAndRezult()
         {
             // Arrange
-            var stringWriter = new StringWriter();
-            Console.SetOut(stringWriter);
-
-            string result = "3\n3\n0\n0\n9\n0";
-            var stringReader = new StringReader(result);
+            string input = "3\n3\n0\n0\n9\n0";
+            var stringReader = new StringReader(input);
             Console.SetIn(stringReader);
 
             // Act
             Program.Main(Array.Empty<string>());
 
             // Assert
-            Assert.Equal("1 -1 3", stringWriter.ToString());
+            Assert.Equal("1 -1 3", _stringWriter.ToString());
         }
 
         [Fact]
         public void IfCoefficientsAreProportional_ThenWriteOneAndRezult()
         {
             // Arrange
-            var stringWriter = new StringWriter();
-            Console.SetOut(stringWriter);
-
-            string result = "1\n1\n3\n3\n3\n9";
-            var stringReader = new StringReader(result);
+            string input = "1\n1\n3\n3\n3\n9";
+            var stringReader = new StringReader(input);
             Console.SetIn(stringReader);
 
             // Act
             Program.Main(Array.Empty<string>());
 
             // Assert
-            Assert.Equal("1 -1 3", stringWriter.ToString());
+            Assert.Equal("1 -1 3", _stringWriter.ToString());
         }
 
         [Fact]
-        public void IfSystemHaveNotSolution_ThenWriteZero()
+        public void IfSystemHasNotSolution_ThenWriteZero()
         {
             // Arrange
-            var stringWriter = new StringWriter();
-            Console.SetOut(stringWriter);
-
-            string result = "1\n1\n3\n3\n33\n9";
-            var stringReader = new StringReader(result);
+            string input = "1\n1\n3\n3\n33\n9";
+            var stringReader = new StringReader(input);
             Console.SetIn(stringReader);
 
             // Act
             Program.Main(Array.Empty<string>());
 
             // Assert
-            Assert.Equal("0", stringWriter.ToString());
+            Assert.Equal("0", _stringWriter.ToString());
         }
 
         [Fact]
-        public void IfABCEeroAndSystemHasSolution_ThenWriteFourAndRezult()
+        public void IfACZeroAndSystemHasNotSolution_ThenWriteZero()
         {
             // Arrange
-            var stringWriter = new StringWriter();
-            Console.SetOut(stringWriter);
-
-            string result = "0\n0\n0\n5\n0\n5";
-            var stringReader = new StringReader(result);
+            string input = "0\n1\n0\n3\n33\n9";
+            var stringReader = new StringReader(input);
             Console.SetIn(stringReader);
 
             // Act
             Program.Main(Array.Empty<string>());
 
             // Assert
-            Assert.Equal("4 1", stringWriter.ToString());
+            Assert.Equal("0", _stringWriter.ToString());
         }
 
         [Fact]
-        public void IfACDFeroAndSystemHasSolution_ThenWriteFourAndRezult()
+        public void IfBDZeroAndSystemHasNotSolution_ThenWriteZero()
         {
             // Arrange
-            var stringWriter = new StringWriter();
-            Console.SetOut(stringWriter);
-
-            string result = "0\n5\n0\n0\n5\n0";
-            var stringReader = new StringReader(result);
+            string input = "1\n0\n3\n0\n33\n9";
+            var stringReader = new StringReader(input);
             Console.SetIn(stringReader);
 
             // Act
             Program.Main(Array.Empty<string>());
 
             // Assert
-            Assert.Equal("4 1", stringWriter.ToString());
+            Assert.Equal("0", _stringWriter.ToString());
         }
 
         [Fact]
-        public void IfABDEeroAndSystemHasSolution_ThenWriteFourAndRezult()
+        public void IfABCDZeroAndSystemHasNotSolution_ThenWriteZero()
         {
             // Arrange
-            var stringWriter = new StringWriter();
-            Console.SetOut(stringWriter);
-
-            string result = "0\n0\n5\n0\n0\n5";
-            var stringReader = new StringReader(result);
+            string input = "0\n0\n0\n0\n33\n9";
+            var stringReader = new StringReader(input);
             Console.SetIn(stringReader);
 
             // Act
             Program.Main(Array.Empty<string>());
 
             // Assert
-            Assert.Equal("3 1", stringWriter.ToString());
+            Assert.Equal("0", _stringWriter.ToString());
         }
 
         [Fact]
-        public void IfBCDFeroAndSystemHasSolution_ThenWriteFourAndRezult()
+        public void IfACEFZeroAndSystemHasNotSolution_ThenWriteFourAndResult()
         {
             // Arrange
-            var stringWriter = new StringWriter();
-            Console.SetOut(stringWriter);
-
-            string result = "5\n0\n0\n0\n5\n0";
-            var stringReader = new StringReader(result);
+            string input = "0\n3\n0\n3\n0\n0";
+            var stringReader = new StringReader(input);
             Console.SetIn(stringReader);
 
             // Act
             Program.Main(Array.Empty<string>());
 
             // Assert
-            Assert.Equal("3 1", stringWriter.ToString());
+            Assert.Equal("4 0", _stringWriter.ToString());
+        }
+
+        [Fact]
+        public void IfBDEFZeroAndSystemHasNotSolution_ThenWriteFourAndResult()
+        {
+            // Arrange
+            string input = "3\n0\n3\n0\n0\n0";
+            var stringReader = new StringReader(input);
+            Console.SetIn(stringReader);
+
+            // Act
+            Program.Main(Array.Empty<string>());
+
+            // Assert
+            Assert.Equal("3 0", _stringWriter.ToString());
         }
     }
 }
--- 6 семестр/ПППП/lab2_csharp/lab2_csharp/Program.cs
+++ 6 семестр/ПППП/lab2_csharp/lab2_csharp/Program.cs
@@ -5,17 +5,17 @@
         public static void Main(string[] args)
         {
             double a, b, c, d, e, f;
-            a = double.Parse(Console.ReadLine());
-            b = double.Parse(Console.ReadLine());
-            c = double.Parse(Console.ReadLine());
-            d = double.Parse(Console.ReadLine());
-            e = double.Parse(Console.ReadLine());
-            f = double.Parse(Console.ReadLine());
-            if (a == 0 && b == 0 && c == 0 && d == 0 && e == 0 && f == 0)
+            double.TryParse(Console.ReadLine(),out a);
+            double.TryParse(Console.ReadLine(),out b);
+            double.TryParse(Console.ReadLine(),out c);
+            double.TryParse(Console.ReadLine(),out d);
+            double.TryParse(Console.ReadLine(),out e);
+            double.TryParse(Console.ReadLine(),out f);
+            if (a == 0 && b == 0 && c == 0 && d == 0 && e == 0 && f == 0) // YES
             {
                 Console.Write("5");
             }
-            else if (a * d - c * b != 0 && (e * d - b * f != 0 || a * f - c * e != 0))
+            else if (a * d - c * b != 0 && (e * d - b * f != 0 || a * f - c * e != 0)) // YES
             {
                 double y = (a * f - c * e) / (a * d - c * b);
                 double x = (d * e - b * f) / (d * a - b * c);
@@ -24,94 +24,94 @@
             else if (a * d - c * b == 0 && (e * d - b * f != 0 || a * f - c * e != 0) ||
                 a == 0 && c == 0 && e / b != f / d ||
                 b == 0 && d == 0 && e / a != f / c ||
-                a == 0 && b == 0 && c == 0 && d == 0 && e / f > 0)
+                a == 0 && b == 0 && c == 0 && d == 0 && e / f > 0) // YES, YES, YES, YES
             {
                 if (a == 0 && b == 0 && e == 0 && d != 0 && c == 0 ||
-                    c == 0 && d == 0 && f == 0 && b != 0 && a == 0)
+                    c == 0 && d == 0 && f == 0 && b != 0 && a == 0) // YES, YES
                 {
                     double y = -8888;
-                    if (b == 0)
+                    if (b == 0) // YES
                         y = f / d;
-                    else if (d == 0)
+                    else if (d == 0) // YES
                         y = e / b;
-                    else if (e == 0 || f == 0)
+                    else if (e == 0 || f == 0) // UNB
                         y = 0;
 
                     Console.Write($"4 {y}");
                 }
                 else if (a == 0 && b == 0 && e == 0 && c != 0 && d == 0 ||
-                    c == 0 && d == 0 && f == 0 && a != 0 && b == 0)
+                    c == 0 && d == 0 && f == 0 && a != 0 && b == 0) // YES, YES
                 {
                     double x = -8888;
-                    if (a == 0)
+                    if (a == 0) // YES
                         x = f / c;
-                    else if (c == 0)
+                    else if (c == 0) // YES
                         x = e / a;
-                    else if (e == 0 || f == 0)
+                    else if (e == 0 || f == 0) // UNB
                         x = 0;
                     Console.Write($"3 {x}");
                 }
-                else
+                else // YES
                     Console.Write("0");
             }
             else if (a == 0 && c == 0)
             {
                 double y;
-                if (e == 0)
+                if (e == 0) // YES
                     y = f / d;
-                else if (f == 0)
+                else if (f == 0) // UNB
                     y = e / b;
-                else
+                else // YES
                     y = e / b;
                 Console.Write($"4 {y}");
             }
             else if (b == 0 && d == 0)
             {
                 double x;
-                if (e == 0)
+                if (e == 0) // YES
                     x = f / c;
-                else if (f == 0)
+                else if (f == 0) // UNB
                     x = e / a;
-                else
+                else // YES
                     x = e / a;
                 Console.Write($"3 {x}");
             }
-            else if (b == 0 && e == 0)
+            else if (b == 0 && e == 0) // YES
             {
                 double k, n;
                 k = -c / d;
                 n = f / d;
                 Console.Write($"1 {k} {n}");
             }
-            else if (d == 0 && f == 0)
+            else if (d == 0 && f == 0) // YES
             {
                 double k, n;
                 k = -a / b;
                 n = e / b;
                 Console.Write($"1 {k} {n}");
             }
-            else if (a == 0 && e == 0)
+            else if (a == 0 && e == 0) // UNB
             {
                 double k, n;
                 k = -d / c;
                 n = f / c;
                 Console.Write($"1 {k} {n}");
             }
-            else if (c == 0 && f == 0)
+            else if (c == 0 && f == 0) // UNB
             {
                 double k, n;
                 k = -b / a;
                 n = e / a;
                 Console.Write($"1 {k} {n}");
             }
-            else if (a / b == c / d)
+            else if (a / b == c / d) // YES
             {
                 double k, n;
                 k = -c / d;
                 n = f / d;
                 Console.Write($"1 {k} {n}");
             }
-            else
+            else // UNB
             {
                 Console.Write("Are you kidding me?");
             }
